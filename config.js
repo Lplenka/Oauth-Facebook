445a30c9ba5272d3aa0e088e7688f23478e284cb
Lplenka <laluprasad.lenka95@gmail.com>
2017-01-10 17:50:10 +0530

    Added all files

---
+++ config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//     'secretKey': '12345-67890-09876-54321',
+//     'mongoUrl' : 'mongodb://localhost:27017/collide'
+// }
+
+
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/collide',
+    'facebook': {
+        clientID: '148980502261590',
+        clientSecret: 'd785d30329fd70903da8c8d168fd84c7',
+        callbackURL: 'http://localhost:3000/users/facebook/callback'
+    }
+}
